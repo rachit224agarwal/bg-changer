8b5415a7fb7c6b8836a0829b527ba4effbb8f69e
rachit224agarwal <agarwalrachit224@gmail.com>
2024-11-27 02:35:40 +0530

    up

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     style={{backgroundColor: color }}  >
       <div className='flex flex-wrap justify-center '>
         <div className='flex flex-wrap gap-3 mt-8 rounded-xl px-4 py-3 outline-none shadow-2xl inset-x-0' style={{backgroundColor: "gray"}}>
-          {/* bg-grey in tailwind CSS */}
+          {/* bg-grey in tailwind CSS  than button bar will also change its color */}
           <button onClick={()=> setColor("red")} className='px-2 py-3 rounded-full text-black bg-red-700' >RED</button>
           <button onClick={()=> setColor("#fc9403")} className='px-2 py-3 rounded-full text-black bg-orange-500'>Orange</button>
           <button onClick={()=> setColor("green")} className='px-2 py-3 rounded-full text-black text-xs bg-green-600' >GREEN</button>
