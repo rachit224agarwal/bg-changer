2fd756664571bda815d904030f11284cc3bb6eda
rachit224agarwal <agarwalrachit224@gmail.com>
2024-11-27 02:29:59 +0530

    update inline css in button

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ const App = () => {
       <div className='flex flex-wrap justify-center '>
         <div className='flex flex-wrap gap-3 mt-8 rounded-xl px-4 py-3 outline-none shadow-2xl inset-x-0' style={{backgroundColor: "gray"}}>
           {/* bg-grey in tailwind CSS */}
-          <button onClick={()=> setColor("red")} className='px-2 py-3 rounded-full text-black ' style={{backgroundColor: "red"}}>RED</button>
-          <button onClick={()=> setColor("#fc9403")} className='px-2 py-3 rounded-full text-black ' style={{backgroundColor: "#fc9403"}}>Orange</button>
-          <button onClick={()=> setColor("green")} className='px-2 py-3 rounded-full text-black text-xs' style={{backgroundColor: "green"}}>GREEN</button>
-          <button onClick={()=> setColor("lightgreen")} className='px-2 py-3 rounded-full text-black text-xs' style={{backgroundColor: "lightgreen"}}>L-GREEN</button>
-          <button onClick={()=> setColor("pink")} className='px-2 py-3 rounded-full text-black text-xs' style={{backgroundColor: "pink"}}>PINK</button>
-          <button onClick={()=> setColor("black")} className='px-2 py-3 rounded-full text-white text-xs' style={{backgroundColor: "black"}}>BLACK</button>
-          <button onClick={()=> setColor("white")} className='px-2 py-3 rounded-full text-black text-xs' style={{backgroundColor: "white"}}>WHITE</button>
-          <button onClick={()=> setColor("yellow")} className='px-2 py-3 rounded-full text-black text-xs' style={{backgroundColor: "yellow"}}>YELLOW</button>
-          <button onClick={()=> setColor("lightyellow")} className='px-2 py-3 rounded-full text-black text-xs' style={{backgroundColor: "lightyellow"}}>L-YELLOW</button>
+          <button onClick={()=> setColor("red")} className='px-2 py-3 rounded-full text-black bg-red-700' >RED</button>
+          <button onClick={()=> setColor("#fc9403")} className='px-2 py-3 rounded-full text-black bg-orange-500'>Orange</button>
+          <button onClick={()=> setColor("green")} className='px-2 py-3 rounded-full text-black text-xs bg-green-600' >GREEN</button>
+          <button onClick={()=> setColor("lightgreen")} className='px-2 py-3 rounded-full text-black text-xs bg-green-300' >L-GREEN</button>
+          <button onClick={()=> setColor("pink")} className='px-2 py-3 rounded-full text-black text-xs bg-pink-500' >PINK</button>
+          <button onClick={()=> setColor("black")} className='px-2 py-3 rounded-full text-white text-xs bg-black'>BLACK</button>
+          <button onClick={()=> setColor("white")} className='px-2 py-3 rounded-full text-black text-xs bg-white'>WHITE</button>
+          <button onClick={()=> setColor("yellow")} className='px-2 py-3 rounded-full text-black text-xs bg-yellow-300'>YELLOW</button>
+          <button onClick={()=> setColor("lightyellow")} className='px-2 py-3 rounded-full text-black text-xs bg-yellow-200'>L-YELLOW</button>
         </div>
       </div>
     </div>
